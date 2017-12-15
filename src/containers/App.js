14864b5a7752ceeb83b98ebf8437414fdc955077
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-15 15:25:16 +0800

    react router dom implemented

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { setEnv_act } from './actions/envActions'
 import { setUser_act } from './actions/userActions'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 class App extends Component {
 
@@ -12,9 +13,29 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-                this is my frontpage
-            </div>
+            <BrowserRouter basename="/homepage/">
+                <Switch>
+
+                    <Route
+                        exact
+                        path='/'
+                        render={props => <div>homepage pls</div>}
+                    />
+
+                    <Route
+                        exact
+                        path='/logout'
+                        render={props => <div>logout pls</div>}
+                    />
+
+                    <Route
+                        exact
+                        path='/login'
+                        render={props => <div>login pls</div>}
+                    />
+
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
