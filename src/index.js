e4c5506ce0f48ad042ec602d2b203618fab6d5d9
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-15 16:07:05 +0800

    semantic ui react package added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './containers/App'
 import store from './store'
 import { Provider } from 'react-redux'
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
     <Provider store={store}>
