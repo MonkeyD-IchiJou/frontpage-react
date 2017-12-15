724e62e135af0b3c9f376aaf27160b6bf90a3e6d
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-15 15:04:00 +0800

    example of connecting to redux from react; mapping state to props and mapping dispatch to props

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { setEnv_act } from './actions/envActions'
+import { setUser_act } from './actions/userActions'
 
 class App extends Component {
+
+    componentDidMount() {
+        this.props.setEnv_act()
+        this.props.setUser_act()
+    }
+
     render() {
         return (
             <div>
                 this is my frontpage
             </div>
-        );
+        )
     }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        envReducer: state.envReducer,
+        userReducer: state.userReducer
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setEnv_act: () => {
+            dispatch(setEnv_act())
+        },
+        setUser_act: () => {
+            dispatch(setUser_act())
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
