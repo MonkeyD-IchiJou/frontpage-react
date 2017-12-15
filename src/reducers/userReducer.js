94230c4a9e5b2c0cb4159fa2e1a03df99e37e6b3
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-15 18:00:44 +0800

    added loginErros and signupErros state for userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,9 +1,49 @@
 const userReducer = (
-    state = { username: '', email: '', jwt: '' },
+    state = { username: '', email: '', jwt: '', loginErrors: '', signupErrors: '' },
     action
 ) => {
 
     switch (action.type) {
+        case "USR_REQ_LOGIN_FULFILLED":
+
+            // set the jwt into my localStorage
+            localStorage.setItem('token', action.payload)
+
+            // set the jwt state
+            state = {
+                ...state,
+                jwt: action.payload,
+                loginErrors: ''
+            }
+
+            break
+
+        case "USR_REQ_LOGIN_REJECTED":
+
+            // rm the jwt in my localStorage
+            localStorage.removeItem('token')
+
+            // set the loginErrors state
+            state = {
+                ...state,
+                jwt: '',
+                loginErrors: action.payload
+            }
+
+            break
+
+        case "USR_REQ_LOGOUT":
+
+            // rm the jwt in my localStorage
+            localStorage.removeItem('token')
+
+            // set the jwt state to null
+            state = {
+                ...state,
+                jwt: ''
+            }
+            break
+
         default:
             break
     }
