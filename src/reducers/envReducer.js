c185bc6f07c4a88ac5ee6e603f8f351b3e7db23c
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-15 14:45:59 +0800

    redux reducer for store action

---
+++ src/reducers/envReducer.js
@@ -0,0 +1,14 @@
+const envReducer = (
+    state = { backendUrl: '' },
+    action
+) => {
+
+    switch (action.type) {
+        default:
+            break
+    }
+
+    return state
+}
+
+export default envReducer
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,14 @@
+const userReducer = (
+    state = { username: '', email: '', jwt: '' },
+    action
+) => {
+
+    switch (action.type) {
+        default:
+            break
+    }
+
+    return state
+}
+
+export default userReducer
