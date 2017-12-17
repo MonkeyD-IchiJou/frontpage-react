56860e377a828175811cde55f07ff45e7dc80127
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-17 23:56:07 +0800

    more actions for env reducers

--- src/containers/actions/envActions.js
+++ src/containers/actions/envActions.js
@@ -1,8 +1,23 @@
+var SetUrlPromise = (backendurl) => {
+    return new Promise((resolve, reject) => {
+        resolve(backendurl)
+    })
+}
+
 export function setUrl_act(backendurl) {
 
     return {
         type: 'SET_BACKEND_URL',
-        payload: backendurl
+        payload: SetUrlPromise(backendurl)
+    }
+
+}
+
+export function setValidatingUser_act(payload) {
+
+    return {
+        type: 'SET_VALIDATING_USR',
+        payload: payload
     }
 
 }
