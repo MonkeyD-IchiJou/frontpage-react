49362f36d48da7a6384999d3b4d5000c006874c9
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-17 23:56:24 +0800

    example login page

--- src/containers/components/Loginpage.js
+++ src/containers/components/Loginpage.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
 
 class Loginpage extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            info: 'kek'
+        }
+    }
+
+    errorCB = (e) => {
+        console.log(e)
+        this.setState({info: e})
+    }
+    successCB = () => {
+        this.props.history.push('/console')
+    }
+
     render() {
         return (
             <div>
                 Login page here
-                <Button>Submit login info</Button>
+                <Button
+                    onClick={() => { 
+                        this.props.ClickLogin('ichijou8282@gmail.com', 'ichijou1234', this.successCB, this.errorCB) 
+                    }}
+                >
+                Submit login info
+                </Button>
+                {this.state.info}
             </div>
         )
     }
