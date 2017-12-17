a3fb9dead2b64c66fe9b21741c5c2aa22b661483
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-17 23:55:55 +0800

    more actions for user reducers

--- src/containers/actions/userActions.js
+++ src/containers/actions/userActions.js
@@ -10,10 +10,10 @@ import request from 'superagent'
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 // user post request login in promise
-var UserRequestLogin = (email, password) => {
+var UserRequestLogin = (backendurl, email, password) => {
     return new Promise((resolve, reject) => {
         request
-            .post('https://localhost/auth/v1/')
+            .post(backendurl + '/auth/v1/')
             .set('contentType', 'application/json; charset=utf-8')
             .set('dataType', 'json')
             .send({
@@ -40,11 +40,11 @@ var UserRequestLogin = (email, password) => {
     })
 }
 
-// user post request to check for jwt valid or not
-/*var JwtRequestValidate = (jwt) => {
+// user post request to get username and email based on jwt
+var RequestUserInfo = (backendurl, jwt) => {
     return new Promise((resolve, reject) => {
         request
-            .post('https://localhost/auth/v1/validate')
+            .post(backendurl + '/user/v1/')
             .set('contentType', 'application/json; charset=utf-8')
             .set('dataType', 'json')
             .send({
@@ -59,29 +59,35 @@ var UserRequestLogin = (email, password) => {
                 else {
                     let result = res.body
 
-                    if (!result || !result.authResult) {
-                        reject('token invalid')
+                    if (!result || !result.success) {
+                        reject('cannot verify user')
                     }
 
-                    resolve(result)
+                    resolve(result.userinfo)
                 }
 
             })
     })
-}*/
+}
 
-// user post request to get username and email based on jwt
-var RequestUserInfo = (jwt) => {
+var CheckTokenPromise = () => {
     return new Promise((resolve, reject) => {
 
+        let jwt = localStorage.getItem('token')
+        if (jwt) {
+            resolve(jwt)
+        }
+        else {
+            reject('no jwt in the localstorage')
+        }
     })
 }
 
 // request login action
-export function reqLogin_act(email, password) {
+export function reqLogin_act(backendurl, email, password) {
     return {
         type: 'USR_REQ_LOGIN',
-        payload: UserRequestLogin(email, password)
+        payload: UserRequestLogin(backendurl, email, password)
     }
 }
 
@@ -90,5 +96,21 @@ export function reqLogout_act() {
     return {
         type: 'USR_REQ_LOGOUT',
         payload: true
+    }
+}
+
+// request getting user info
+export function reqUserInfo_act(backendurl, jwt) {
+    return {
+        type: 'USR_REQ_INFO',
+        payload: RequestUserInfo(backendurl, jwt)
+    }
+}
+
+// request to see whether got any token in localstorage or not
+export function reqCheckToken_act() {
+    return {
+        type: 'REQ_CHECKTOKEN_STORAGE',
+        payload: CheckTokenPromise()
     }
 }
