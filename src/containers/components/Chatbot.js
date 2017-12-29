8586cbb19f2b68d39a847a66740502b83b02e626
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-29 18:27:18 +0800

    Dashboard dump component added

--- src/containers/components/Chatbot.js
+++ src/containers/components/Chatbot.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 class Chatbot extends Component {
+    componentDidMount() {
+        // change the header title to dashboard
+        this.props.changeTitle('Chatbot-afadfasf')
+    }
     render() {
         return (
             <div>Chatbot</div>
--- src/containers/components/Dashboard.js
+++ src/containers/components/Dashboard.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import UsrDashboard from './UsrDashboard'
+import CbDashboard from './CbDashboard'
+import LcDashboard from './LcDashboard'
+import { Grid } from 'semantic-ui-react'
 
 class Dashboard extends Component {
+
+    componentDidMount() {
+        // change the header title to dashboard
+        this.props.changeTitle('Dashboard')
+    }
+
     render() {
-        return(
-            <div>Dashboard</div>
+        return (
+            <Grid stackable columns='equal' ref={this.handleContextRef}>
+
+                <Grid.Column>
+                    <UsrDashboard />
+                </Grid.Column>
+
+                <Grid.Column>
+                    <CbDashboard />
+                </Grid.Column>
+
+                <Grid.Column>
+                    <LcDashboard />
+                </Grid.Column>
+
+            </Grid>
         )
     }
 }
