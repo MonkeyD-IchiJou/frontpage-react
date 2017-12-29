9983fcd1f8ed21773a416d4650605c5c3298dd0b
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-29 15:17:47 +0800

    added new reducers to the store

--- src/store.js
+++ src/store.js
@@ -4,13 +4,15 @@ import thunk from 'redux-thunk'
 import promiseMiddleware from 'redux-promise-middleware'
 import envReducer from './reducers/envReducer'
 import userReducer from './reducers/userReducer'
+import chatbotReducer from './reducers/chatbotReducer'
+import livechatReducer from './reducers/livechatReducer'
 
 const logger = createLogger({
     collapsed: true
 })
 
 export default createStore(
-    combineReducers({ envReducer, userReducer }),
+    combineReducers({ envReducer, userReducer, chatbotReducer, livechatReducer }),
     {},
     applyMiddleware(logger, thunk, promiseMiddleware())
 )
