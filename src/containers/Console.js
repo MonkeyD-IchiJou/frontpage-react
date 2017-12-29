01c2a3008ccac7b11489fe71d7e720dae555152f
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-29 18:28:56 +0800

    console container updated

--- src/containers/Console.js
+++ src/containers/Console.js
@@ -4,30 +4,47 @@ import { Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import Chatbot from './components/Chatbot'
 import Livechat from './components/Livechat'
+import ConsoleHeader from './components/ConsoleHeader'
+import { Container } from 'semantic-ui-react'
 
 class Console extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: 'default'
+        }
+    }
+
+    changeTitle = (title) => {
+        this.setState({title: title})
+    }
+
     render() {
 
         const { match } = this.props
 
         return (
-            <div>
+            <Container>
+                
+                <ConsoleHeader ClickLogout={this.props.ClickLogout} history={this.props.history} title={this.state.title}/>
+                
                 <Route 
                     exact 
                     path={`${match.url}/`} 
-                    render={props => <Dashboard {...props} />}
+                    render={props => <Dashboard {...props} changeTitle={this.changeTitle}/>}
                 />
                 <Route
                     exact
                     path={`${match.url}/chatbot`}
-                    render={props => <Chatbot {...props} />}
+                    render={props => <Chatbot {...props} changeTitle={this.changeTitle}/>}
                 />
                 <Route
                     exact
                     path={`${match.url}/livechat`}
-                    render={props => <Livechat {...props} />}
+                    render={props => <Livechat {...props} changeTitle={this.changeTitle}/>}
                 />
-            </div>
+            </Container>
         )
     }
 }
