b5a64cdb0dc278bcffe8e19e17bd762fedf88f81
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-29 15:16:52 +0800

    new dumb components for console

---
+++ src/containers/components/Chatbot.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Chatbot extends Component {
+    render() {
+        return (
+            <div>Chatbot</div>
+        )
+    }
+}
+
+export default Chatbot
---
+++ src/containers/components/Livechat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Livechat extends Component {
+    render() {
+        return (
+            <div>Livechat</div>
+        )
+    }
+}
+
+export default Livechat
