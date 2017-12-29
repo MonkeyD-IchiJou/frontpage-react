896c32a876ef1ce5bb1c8df4ff468188ba6a0462
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-29 15:17:35 +0800

    Console container init setup

--- src/containers/Console.js
+++ src/containers/Console.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Chatbot from './components/Chatbot'
+import Livechat from './components/Livechat'
 
 class Console extends Component {
-
     render() {
+
+        const { match } = this.props
+
         return (
             <div>
-                Console
-                <Button onClick={() => {
-                    this.props.ClickLogout()
-                }}>
-                    Logout
-                </Button>
+                <Route 
+                    exact 
+                    path={`${match.url}/`} 
+                    render={props => <Dashboard {...props} />}
+                />
+                <Route
+                    exact
+                    path={`${match.url}/chatbot`}
+                    render={props => <Chatbot {...props} />}
+                />
+                <Route
+                    exact
+                    path={`${match.url}/livechat`}
+                    render={props => <Livechat {...props} />}
+                />
             </div>
         )
     }
 }
 
-export default Console
+const mapStateToProps = (state) => {
+    return {
+        chatbotReducer: state.chatbotReducer,
+        livechatReducer: state.livechatReducer
+    }
+}
+
+export default connect(mapStateToProps)(Console)
