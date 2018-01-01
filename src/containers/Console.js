d63a350ae2b2aa050c6e6e26c0dfd1e705949e74
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-01 22:22:22 +0800

    admin joining chatbots room to listen how many clients are online

--- src/containers/Console.js
+++ src/containers/Console.js
@@ -5,7 +5,7 @@ import Dashboard from './components/Dashboard'
 import Chatbot from './components/Chatbot'
 import Livechat from './components/Livechat'
 import ConsoleHeader from './components/ConsoleHeader'
-import { reqChatbotsInfos_act } from './actions/chatbotsActions'
+import { reqChatbotsInfos_act, chatbotClientsListUpdate_act } from './actions/chatbotsActions'
 import { Container } from 'semantic-ui-react'
 
 class Console extends Component {
@@ -27,7 +27,43 @@ class Console extends Component {
 
     getAllChatbotsInfo = () => {
         let userReducer = this.props.userReducer
-        this.props.dispatch(reqChatbotsInfos_act(this.props.backendUrl, userReducer.jwt))
+        this.props.dispatch(reqChatbotsInfos_act(this.props.backendUrl, userReducer.jwt)).then((result) => {
+            this.connectChatbots()
+            // get domain, stories and nlu too
+        }).catch((e) => {
+            console.log(e)
+        })
+    }
+
+    connectChatbots = () => {
+        let backendUrl = this.props.backendUrl
+        let chatbotsReducer = this.props.chatbotsReducer
+
+        for(let i = 0; i < chatbotsReducer.length; ++i) {
+            let chatbotSocket = chatbotsReducer[i].chatbotSocket
+            let roomId = chatbotsReducer[i].uuid
+
+            // connect to my socket server
+            chatbotSocket.connectSocket(backendUrl + '/cbIO')
+
+            // my chatbot socket server subscription
+            chatbotSocket.subscribe('connect', () => {
+                // first, asking to join my chatbot room
+                chatbotSocket.socketEmit('admin_join_room', {
+                    roomId: roomId
+                })
+
+                chatbotSocket.subscribe('admin_joined', (data) => {
+                    // client successfully joined the room liao
+                })
+
+                chatbotSocket.subscribe('clientlist_update', (data) => {
+                    // when there are someone connect to this chatbot, admin will get notified
+                    this.props.dispatch(chatbotClientsListUpdate_act(i, data.clientsInfo))
+                })
+
+            })
+        }
     }
 
     render() {
@@ -44,9 +80,8 @@ class Console extends Component {
                     render={props => <Dashboard {...props} changeTitle={this.changeTitle} userReducer={this.props.userReducer} chatbotsReducer={this.props.chatbotsReducer}/>}
                 />
                 <Route
-                    exact
                     path={`${match.url}/chatbot`}
-                    render={props => <Chatbot {...props} changeTitle={this.changeTitle}/>}
+                    render={props => <Chatbot {...props} changeTitle={this.changeTitle} chatbotsReducer={this.props.chatbotsReducer}/>}
                 />
                 <Route
                     exact
@@ -61,7 +96,8 @@ class Console extends Component {
 const mapStateToProps = (state) => {
     return {
         chatbotsReducer: state.chatbotsReducer,
-        livechatReducer: state.livechatReducer
+        livechatReducer: state.livechatReducer,
+        cbclients: state.chatbotsReducer.clientsList
     }
 }
 
