23880fd694240dd4aaa271229bb013b5da007699
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-01 23:16:08 +0800

    storing each domain, nlu_data and stories for each chatbot

--- src/reducers/chatbotsReducer.js
+++ src/reducers/chatbotsReducer.js
@@ -18,6 +18,15 @@ const chatbotsReducer = (
             state[action.payload.cbindex].clientsList = action.payload.clientsList
             break
 
+        case "USR_REQ_CHATBOT_ML_DATA_FULFILLED":
+            state = [
+                ...state
+            ]
+            state[action.payload.cbindex].domain = action.payload.domain
+            state[action.payload.cbindex].nlu_data = action.payload.nlu_data
+            state[action.payload.cbindex].stories = action.payload.stories
+            break
+
         default:
             break
     }
