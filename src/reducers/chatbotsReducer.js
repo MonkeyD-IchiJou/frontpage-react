0d13ca781d972cd163451d5926ff1ac0b03ca72c
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-01 22:21:15 +0800

    immutable states implementation

--- src/reducers/chatbotsReducer.js
+++ src/reducers/chatbotsReducer.js
@@ -5,7 +5,17 @@ const chatbotsReducer = (
     switch (action.type) {
 
         case "USR_REQ_CHATBOTS_FULFILLED":
+            state = [
+                ...state
+            ]
             state = action.payload
+            break
+
+        case "CHATBOT_UPDATE_CLIENTS":
+            state = [
+                ...state
+            ]
+            state[action.payload.cbindex].clientsList = action.payload.clientsList
             break
 
         default:
