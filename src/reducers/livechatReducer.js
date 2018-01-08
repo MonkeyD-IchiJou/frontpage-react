41ddba79b65cf624614b7b373683187b025d1f95
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-08 19:42:58 +0800

    livechat reducers updated;

--- src/reducers/livechatReducer.js
+++ src/reducers/livechatReducer.js
@@ -3,8 +3,24 @@ const livechatReducer = (
     action
 ) => {
     switch (action.type) {
+
+        case "USR_REQ_LIVECHATS_FULFILLED":
+            state = [
+                ...state
+            ]
+            state = action.payload
+            break
+
+        case "LIVECHAT_UPDATE_CLIENTS":
+            state = [
+                ...state
+            ]
+            state[action.payload.cbindex].clientsList = action.payload.clientsList
+            break
+
         default:
             break
+
     }
 
     return state
