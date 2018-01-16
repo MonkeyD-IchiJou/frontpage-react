e97a11aaffdc2b23bdfd817bb06befb70ba74d86
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-16 16:37:33 +0800

    a pop up confirm page component for action double confirmation

---
+++ src/containers/components/ConfirmRemove.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Button, Confirm, Icon } from 'semantic-ui-react'
+
+class ConfirmRemove extends Component {
+    state = { open: false }
+
+    show = () => this.setState({ open: true })
+
+    handleConfirm = () => {
+        this.props.confirmAction()
+        this.setState({ open: false })
+    }
+
+    handleCancel = () => this.setState({ open: false })
+
+    render() {
+        return (
+            <div>
+                <Button icon basic negative floated='right' size='small' onClick={this.show}>
+                    <Icon name='minus' />
+                </Button>
+                <Confirm
+                    header='Warning'
+                    content='Are you sure to remove this'
+                    open={this.state.open}
+                    onCancel={this.handleCancel}
+                    onConfirm={this.handleConfirm}
+                />
+            </div>
+        )
+    }
+}
+
+export default ConfirmRemove
