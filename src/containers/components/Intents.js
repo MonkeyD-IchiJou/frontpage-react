3c5288d2989c580c4b931581cec25d479ca36be3
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-17 15:47:41 +0800

    component to display intents and create a new intent

--- src/containers/components/Intents.js
+++ src/containers/components/Intents.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react'
-import { Table, Icon, Button } from 'semantic-ui-react'
+import Intent from './../../classes/Intent'
+import EditIntent from './EditIntent'
+import ConfirmRemove from './ConfirmRemove'
+import { Table, Pagination, Form } from 'semantic-ui-react'
 
 class Intents extends Component {
 
     constructor(props) {
         super(props)
-
-        // init my state at the very begining
         this.state = {
-            intents: this.props.cbIntents
+            activePage: 1,
+            newintent: ''
         }
     }
 
+    handlePaginationChange = (e, { activePage }) => this.setState({ activePage })
+
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
     render() {
 
-        let displayIntents = this.state.intents.map((intent, index) => {
+        let intents = [...this.props.cbIntents]
+        let updateIntents = this.props.updateIntents
+        const { activePage, newintent } = this.state
+
+        // first see how many pages
+        let totalpages = Math.ceil(intents.length / 10.0)
+        let displayPagination = ''
+        if (totalpages > 1) {
+            displayPagination = (<Pagination activePage={activePage} onPageChange={this.handlePaginationChange} totalPages={totalpages} />)
+        }
+
+        const sliceStartId = (activePage - 1) * 10
+        const sliceEndId = activePage * 10
+
+        let displayIntents = intents.slice(sliceStartId, sliceEndId).map((intent, index) => {
+            // the real index
+            index += sliceStartId
             return (
                 <Table.Row key={index}>
 
@@ -23,9 +45,18 @@ class Intents extends Component {
                     </Table.Cell>
 
                     <Table.Cell>
-                        <Button icon basic floated='right' size='small'>
-                            <Icon name='write' />
-                        </Button>
+                        <ConfirmRemove confirmAction={() => {
+                            // remove this intent
+                            intents.splice(index, 1)
+                            // then update my redux store
+                            updateIntents(intents)
+                        }}/>
+                        <EditIntent intent={intent} availableEntities= {this.props.cbEntities} updateIntent={(newintent) => {
+                            // update this specific intent
+                            intents[index] = newintent
+                            // then update my redux store
+                            updateIntents(intents)
+                        }} />
                     </Table.Cell>
 
                 </Table.Row>
@@ -49,9 +80,16 @@ class Intents extends Component {
                 <Table.Footer fullWidth>
                     <Table.Row>
                         <Table.HeaderCell colSpan='4'>
-                            <Button floated='right' icon labelPosition='left' primary size='small'>
-                                <Icon name='plus' /> Add Intent
-                            </Button>
+                            <Form onSubmit={() => {
+                                // create a default new entity
+                                intents.push(new Intent(newintent, [], []))
+                                // then update my redux store
+                                updateIntents(intents)
+                                this.setState({ newintent: '' })
+                            }}>
+                                <Form.Input placeholder='Create New Intent' name='newintent' value={newintent} onChange={this.handleChange} />
+                            </Form>
+                            {displayPagination}
                         </Table.HeaderCell>
                     </Table.Row>
                 </Table.Footer>
