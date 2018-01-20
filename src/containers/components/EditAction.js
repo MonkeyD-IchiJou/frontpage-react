0f80bb795e8d20b1f0d72f6429f8d64dd4820366
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-20 23:26:50 +0800

    error handling and prompt msg when usr has not saved

--- src/containers/components/EditAction.js
+++ src/containers/components/EditAction.js
@@ -2,32 +2,51 @@ import React, { Component } from 'react'
 import ProgressSave from './ProgressSave'
 import EditSingleAction from './EditSingleAction'
 import TextResponse from './../classes/TextResponse'
+import { Prompt } from 'react-router-dom'
 import { Button, Icon, Header, Input, Divider } from 'semantic-ui-react'
 
 class EditAction extends Component {
 
     constructor(props) {
         super(props)
+
         this.state = {
-            actionName: this.props.action.name,
-            allActions: JSON.parse(JSON.stringify(this.props.action.allActions))
+            actionName: '',
+            allActions: [],
+            hasSaved: true
+        }
+
+        if(this.props.action) {
+            this.state = {
+                actionName: this.props.action.name,
+                allActions: JSON.parse(JSON.stringify(this.props.action.allActions)),
+                hasSaved: true
+            }
         }
     }
 
     componentWillReceiveProps = (nextProps) => {
-        this.setState({
-            actionName: nextProps.action.name,
-            allActions: JSON.parse(JSON.stringify(nextProps.action.allActions))
-        })
+        if(nextProps.action) {
+            this.setState({
+                actionName: nextProps.action.name,
+                allActions: JSON.parse(JSON.stringify(nextProps.action.allActions)),
+                hasSaved: true
+            })
+        }
+    }
+
+    editChanges = (states) => {
+        this.setState({ ...states, hasSaved: false })
     }
 
     render() {
 
-        let { actionName, allActions } = this.state
+        let { actionName, allActions, hasSaved } = this.state
 
         return(
-            
             <div style={{padding: '10px'}}>
+
+                <Prompt when={!hasSaved} message="Warning! All the progress will be lost if you leave this place" />
 
                 <ProgressSave
                     clickDone={() => {
@@ -38,7 +57,7 @@ class EditAction extends Component {
                 <Header>Action Name</Header>
 
                 <Input value={actionName} fluid onChange={(event, data) => {
-                    this.setState({ actionName: data.value })
+                    this.editChanges({ actionName: data.value })
                 }} />
 
                 <Header>Actions</Header>
@@ -49,11 +68,11 @@ class EditAction extends Component {
                             actions={actions}
                             removeAction={() => {
                                 allActions.splice(index, 1)
-                                this.setState({ allActions: allActions })
+                                this.editChanges({ allActions: allActions })
                             }}
                             updateActions={(actions)=>{
                                 allActions[index] = actions
-                                this.setState({ allActions: allActions })
+                                this.editChanges({ allActions: allActions })
                             }}
                             key={index} 
                         />
@@ -66,7 +85,7 @@ class EditAction extends Component {
 
                 <Button primary onClick={()=>{
                     allActions.push([new TextResponse('')])
-                    this.setState({ allActions: allActions })
+                    this.editChanges({ allActions: allActions })
                 }}>
                     <Icon name='plus'/>Create New Action
                 </Button>
@@ -77,18 +96,3 @@ class EditAction extends Component {
 }
 
 export default EditAction
-
-/*
-<Modal
-    trigger={
-        <div>
-            <ConfirmRemove confirmAction={()=>{this.props.removeActions()}} />
-            <span style={{ cursor: 'pointer' }} onClick={this.handleOpen}>{actionName}</span>
-        </div>
-    }
-    open={this.state.modalOpen}
-    onClose={this.handleClose}
-    closeOnDimmerClick={false}
-    size="fullscreen"
->
-*/
--- src/containers/components/MasterEditAction.js
+++ src/containers/components/MasterEditAction.js
@@ -6,20 +6,14 @@ class MasterEditAction extends Component {
     render() {
 
         const { cbActions, match, updateActions } = this.props
-
-        // deep clone this all actions
-        let actions = JSON.parse(JSON.stringify(cbActions))
-
-        // get the index of the action i want to edit
         let index = match.params.topicId
-
-        // get the hold of the chosen action
-        let action = actions[match.params.topicId]
 
         return (
             <EditAction
-                action={action}
+                action={cbActions[index]}
                 updateActions={(newAction) => {
+                    // deep clone this all actions
+                    let actions = JSON.parse(JSON.stringify(cbActions))
                     actions[index] = newAction
                     updateActions(actions)
                 }}
