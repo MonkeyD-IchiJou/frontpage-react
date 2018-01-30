fa7cd6e8ff18ab9d4c3fb810173e987691020b5a
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-30 17:47:38 +0800

    able to clicked and checked intents for further action

--- src/containers/components/DisplayIntentsTable.js
+++ src/containers/components/DisplayIntentsTable.js
@@ -2,24 +2,39 @@ import React, { Component } from 'react'
 import Intent from './../classes/Intent'
 import FooterForm from './FooterForm'
 import ConfirmRemove from './ConfirmRemove'
-import { Table, Pagination } from 'semantic-ui-react'
+import { Table, Pagination, Checkbox, Dropdown } from 'semantic-ui-react'
 
 class DisplayIntentsTable extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            activePage: 1
+            activePage: 1,
+            allCheckedItems: []
         }
     }
 
     handlePaginationChange = (e, { activePage }) => this.setState({ activePage })
 
+    PushCheckedItem = (itemName) => {
+        this.setState({allCheckedItems: [...this.state.allCheckedItems, itemName]})
+    }
+
+    PopCheckedItem = (itemName) => {
+        let newItemlist = []
+        this.state.allCheckedItems.forEach((item)=>{
+            if(item !== itemName) {
+                newItemlist.push(item)
+            }
+        })
+        this.setState({ allCheckedItems: newItemlist })
+    }
+
     render() {
 
         const { updateIntents, cbIntents, history, match, } = this.props
         let intents = JSON.parse(JSON.stringify(cbIntents))
-        const { activePage } = this.state
+        const { activePage, allCheckedItems } = this.state
 
         // first see how many pages
         let totalpages = Math.ceil(intents.length / 10.0)
@@ -32,41 +47,81 @@ class DisplayIntentsTable extends Component {
         const sliceEndId = activePage * 10
 
         return (
-            <Table selectable>
+            <div>
+                <Dropdown text='Actions'>
+                    <Dropdown.Menu>
+                        <Dropdown.Item icon='share' text='Move to other Subdomain' onClick={()=>{
+                            console.log('click move to other domain')
+                        }}/>
+                        <Dropdown.Divider />
+                        <Dropdown.Item icon='trash' text='Remove' onClick={()=>{
+                            for (let i = 0; i < intents.length; i++) {
+                                for(let j = 0; j < allCheckedItems.length; ++j) {
+                                    if (intents[i].intent === allCheckedItems[j]) {
+                                        intents.splice(i, 1)
+                                    }
+                                }
+                            }
+                            updateIntents(intents)
+                            this.setState({allCheckedItems: []})
+                        }}/>
+                    </Dropdown.Menu>
+                </Dropdown>
 
-                <Table.Body>
-                    {intents.slice(sliceStartId, sliceEndId).map((intent, index) => {
-                        // the real index
-                        index += sliceStartId
-                        return (
-                            <Table.Row key={index}>
-                                <Table.Cell>
-                                    <ConfirmRemove confirmAction={() => {
-                                        // remove this intent
-                                        intents.splice(index, 1)
-                                        // then update my redux store
-                                        updateIntents(intents)
-                                    }} />
-                                    <span style={{ cursor: 'pointer' }} onClick={() => { history.push(`${match.url}/${index}`) }}>{intent.intent}</span>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
-                </Table.Body>
+                <Table selectable>
 
-                <Table.Footer fullWidth>
-                    <Table.Row>
-                        <Table.HeaderCell>
-                            <FooterForm placeholder='Create New Intent' formSubmit={(formvalue) => {
-                                intents.push(new Intent(formvalue, [], []))
-                                updateIntents(intents)
-                            }} />
-                            {displayPagination}
-                        </Table.HeaderCell>
-                    </Table.Row>
-                </Table.Footer>
+                    <Table.Body>
+                        {intents.slice(sliceStartId, sliceEndId).map((intent, index) => {
+                            // the real index
+                            index += sliceStartId
 
-            </Table>
+                            // whether the item has been checked previously or not
+                            let checkedOrNot = false
+                            allCheckedItems.forEach((item) => {
+                                if (item === intent.intent) {
+                                    checkedOrNot = true
+                                }
+                            })
+
+                            return (
+                                <Table.Row key={index}>
+                                    <Table.Cell>
+                                        <ConfirmRemove confirmAction={() => {
+                                            // remove this intent
+                                            intents.splice(index, 1)
+                                            // then update my redux store
+                                            updateIntents(intents)
+                                        }} />
+                                        <Checkbox name={intent.intent} checked={checkedOrNot} onClick={(event, data)=>{
+                                            if(data.checked) {
+                                                this.PushCheckedItem(data.name)
+                                            }
+                                            else {
+                                                this.PopCheckedItem(data.name)
+                                            }
+                                        }}/>
+                                        &nbsp; &nbsp;
+                                        <span style={{ cursor: 'pointer' }} onClick={() => { history.push(`${match.url}/${index}`) }}>{intent.intent}</span>
+                                    </Table.Cell>
+                                </Table.Row>
+                            )
+                        })}
+                    </Table.Body>
+
+                    <Table.Footer fullWidth>
+                        <Table.Row>
+                            <Table.HeaderCell>
+                                <FooterForm placeholder='Create New Intent' formSubmit={(formvalue) => {
+                                    intents.push(new Intent(formvalue, [], []))
+                                    updateIntents(intents)
+                                }} />
+                                {displayPagination}
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Footer>
+
+                </Table>
+            </div>
         )
     }
 
