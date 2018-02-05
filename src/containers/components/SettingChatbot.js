db5c6495ab47ba1e9e7b72a43b41539329299541
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-02-05 17:29:10 +0800

    basic setting page ui updated

--- src/containers/components/SettingChatbot.js
+++ src/containers/components/SettingChatbot.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-import { Segment } from 'semantic-ui-react'
+import { Segment, Item } from 'semantic-ui-react'
 
 class SettingChatbot extends Component {
     render() {
+        const { backendUrl, chatbotInfo } = this.props
+
         return (
             <Segment>
-                Settings
+                <Item.Group>
+                    <Item>
+                        <Item.Content>
+                            <Item.Header>{chatbotInfo.name}</Item.Header>
+                            <Item.Description>
+                                {chatbotInfo.description}
+                            </Item.Description>
+                            <Item.Extra><a href={backendUrl + '/render/?botToken=' + chatbotInfo.uuid} target="_blank">{chatbotInfo.uuid}</a></Item.Extra>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
             </Segment>
         )
     }
