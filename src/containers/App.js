87369b72000a77407262763c12027edabe0a82b1
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-02-07 16:08:35 +0800

    code beautified

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,163 +10,163 @@ import Console from './Console'
 import Uploadpage from './components/Uploadpage'
 
 const PrivateRoute = ({ component: Component, confirmLogin: loginornot, compProps: ComponentProps, ...rest }) => {
-
-    return (
-        <Route
-            {...rest}
-            render={
-                props => (
-                    loginornot ? (<Component {...props} {...ComponentProps} />) : (
-                        <Redirect to={{
-                            pathname: '/login',
-                            state: { from: props.location }
-                        }} />
-                    )
-                )
-            }
-        />
-    )
-
+  return (
+    <Route
+      {...rest}
+      render={
+        props => (
+          loginornot ? (<Component {...props} {...ComponentProps} />) : (
+            <Redirect to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }} />
+          )
+        )
+      }
+    />
+  )
 }
 
 class App extends Component {
 
-    componentDidMount() {
-        // at the very beginning, I am first validating the user
-        this.FirstCheckUser()
+  componentDidMount() {
+    // at the very beginning, I am first validating the user
+    this.FirstCheckUser()
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // do not render my component if i am loading my app
+    return !nextProps.envReducer.apploading
+  }
+
+  FirstCheckUser = async () => {
+    try {
+
+      // see whether got jwt token stored in the storage or not
+      await this.props.dispatch(reqCheckToken_act())
+
+      let backendUrl = this.props.envReducer.backendUrl
+      let jwt = this.props.userReducer.jwt
+
+      // get user info based on jwt
+      await this.props.dispatch(reqUserInfo_act(backendUrl, jwt))
+
+      // finish validating the user
+      this.props.dispatch(setAppLoading_act(false))
+
+    } catch (e) {
+      // finish validating the user
+      this.props.dispatch(setAppLoading_act(false))
     }
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        // do not render my component if i am loading my app
-        return !nextProps.envReducer.apploading
-    }
+  ClickLogout = () => {
+    // user log out
+    this.props.dispatch(reqLogout_act(this.props.envReducer.backendUrl))
+  }
 
-    FirstCheckUser = async () => {
-        try {
+  ClickLogin = async (email, password, successCB, errorCB) => {
 
-            // see whether got jwt token stored in the storage or not
-            await this.props.dispatch(reqCheckToken_act())
+    // loading screen start
+    this.props.dispatch(setAppLoading_act(true))
 
-            let backendUrl = this.props.envReducer.backendUrl
-            let jwt = this.props.userReducer.jwt
+    try {
 
-            // get user info based on jwt
-            await this.props.dispatch(reqUserInfo_act(backendUrl, jwt))
+      // get the backend url from my props
+      let backendUrl = this.props.envReducer.backendUrl
 
-            // finish validating the user
-            this.props.dispatch(setAppLoading_act(false))
+      // user login request
+      await this.props.dispatch(reqLogin_act(backendUrl, email, password))
 
-        } catch (e) {
-            // finish validating the user
-            this.props.dispatch(setAppLoading_act(false))
-        }
-    }
+      // get user info based on jwt
+      await this.props.dispatch(reqUserInfo_act(backendUrl, this.props.userReducer.jwt))
 
-    ClickLogout = () => {
-        // user log out
-        this.props.dispatch(reqLogout_act(this.props.envReducer.backendUrl))
-    }
+      // finish loading
+      this.props.dispatch(setAppLoading_act(false))
 
-    ClickLogin = async (email, password, successCB, errorCB) => {
-
-        // loading screen start
-        this.props.dispatch(setAppLoading_act(true))
-
-        try {
-
-            // get the backend url from my props
-            let backendUrl = this.props.envReducer.backendUrl
-
-            // user login request
-            await this.props.dispatch(reqLogin_act(backendUrl, email, password))
-
-            // get user info based on jwt
-            await this.props.dispatch(reqUserInfo_act(backendUrl, this.props.userReducer.jwt))
-
-            // finish loading
-            this.props.dispatch(setAppLoading_act(false))
-
-            // login successfully liao
-            successCB()
-
-        } catch (e) {
-
-            // finish loading
-            this.props.dispatch(setAppLoading_act(false))
-
-            // login error, componenet callback do something about it
-            errorCB(e.toString())
-        }
-    }
-
-    render() {
-
-        // do not render anything If I am loading my app
-        if (!this.props.envReducer.apploading) {
-
-            // whether this user has really logged in before
-            let confirmLogin = this.props.userReducer.uservalidate
-            let envReducer = this.props.envReducer
-
-            return (
-                <BrowserRouter basename="/homepage/">
-                    <Switch>
-
-                        <Route
-                            exact
-                            path='/'
-                            render={props => <Homepage {...props} />}
-                        />
-
-                        <Route
-                            exact
-                            path='/login'
-                            render={props => <Loginpage {...props} ClickLogin={this.ClickLogin} />}
-                        />
-
-                        <Route
-                            exact
-                            path='/signup'
-                            render={props => <Signuppage {...props} />}
-                        />
-
-                        <Route
-                            exact
-                            path='/upload'
-                            render={props => <Uploadpage {...props} backendUrl={envReducer.backendUrl}/>}
-                        />
-
-                        <PrivateRoute
-                            path='/console'
-                            component={Console}
-                            confirmLogin={confirmLogin}
-                            compProps={{ 
-                                ClickLogout: this.ClickLogout,
-                                userReducer: this.props.userReducer,
-                                backendUrl: envReducer.backendUrl
-                            }}
-                        />
-
-                    </Switch>
-                </BrowserRouter>
-            )
-        }
-        else{
-            return(
-                <div>
-                    Validating User.. loading screen
-                </div>
-            )
-        }
+      // login successfully liao
+      successCB()
 
     }
+    catch (e) {
+
+      // finish loading
+      this.props.dispatch(setAppLoading_act(false))
+
+      // login error, componenet callback do something about it
+      errorCB(e.toString())
+
+    }
+  }
+
+  render() {
+
+    // do not render anything If I am loading my app
+    if (!this.props.envReducer.apploading) {
+
+      // whether this user has really logged in before
+      let confirmLogin = this.props.userReducer.uservalidate
+      let envReducer = this.props.envReducer
+
+      return (
+        <BrowserRouter basename="/homepage/">
+          <Switch>
+
+            <Route
+              exact
+              path='/'
+              render={props => <Homepage {...props} />}
+            />
+
+            <Route
+              exact
+              path='/login'
+              render={props => <Loginpage {...props} ClickLogin={this.ClickLogin} />}
+            />
+
+            <Route
+              exact
+              path='/signup'
+              render={props => <Signuppage {...props} />}
+            />
+
+            <Route
+              exact
+              path='/upload'
+              render={props => <Uploadpage {...props} backendUrl={envReducer.backendUrl} />}
+            />
+
+            <PrivateRoute
+              path='/console'
+              component={Console}
+              confirmLogin={confirmLogin}
+              compProps={{
+                ClickLogout: this.ClickLogout,
+                userReducer: this.props.userReducer,
+                backendUrl: envReducer.backendUrl
+              }}
+            />
+
+          </Switch>
+        </BrowserRouter>
+      )
+    }
+    else {
+      return (
+        <div>
+          Validating User.. loading screen
+        </div>
+      )
+    }
+
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        envReducer: state.envReducer,
-        userReducer: state.userReducer
-    }
+  return {
+    envReducer: state.envReducer,
+    userReducer: state.userReducer
+  }
 }
 
 export default connect(mapStateToProps)(App)
--- src/containers/Console.js
+++ src/containers/Console.js
@@ -8,75 +8,75 @@ import { Container } from 'semantic-ui-react'
 
 class Console extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: 'default'
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'default'
     }
+  }
 
-    changeTitle = (title) => {
-        this.setState({title: title})
-    }
+  changeTitle = (title) => {
+    this.setState({ title: title })
+  }
 
-    render() {
-        const { 
-            match,
-            ClickLogout,
-            history,
-            userReducer,
-            backendUrl
-        } = this.props
+  render() {
+    const {
+      match,
+      ClickLogout,
+      history,
+      userReducer,
+      backendUrl
+    } = this.props
 
-        const jwt = userReducer.jwt
+    const jwt = userReducer.jwt
 
-        return (
-            <Container>
+    return (
+      <Container>
 
-                <ConsoleHeader ClickLogout={ClickLogout} history={history} title={this.state.title}/>
+        <ConsoleHeader ClickLogout={ClickLogout} history={history} title={this.state.title} />
 
-                <Route
-                    exact
-                    path={`${match.url}/`}
-                    render={
-                        props => <Dashboard
-                            {...props}
-                            changeTitle={this.changeTitle}
-                            userReducer={userReducer}
-                            backendUrl={backendUrl}
-                        />
-                    }
-                />
+        <Route
+          exact
+          path={`${match.url}/`}
+          render={
+            props => <Dashboard
+              {...props}
+              changeTitle={this.changeTitle}
+              userReducer={userReducer}
+              backendUrl={backendUrl}
+            />
+          }
+        />
 
-                <Route
-                    path={`${match.url}/chatbot/:topicId`}
-                    render={
-                        props => <Chatbot
-                            {...props}
-                            jwt={jwt}
-                            usremail={userReducer.email}
-                            backendUrl={backendUrl}
-                            changeTitle={this.changeTitle}
-                        />
-                    }
-                />
+        <Route
+          path={`${match.url}/chatbot/:topicId`}
+          render={
+            props => <Chatbot
+              {...props}
+              jwt={jwt}
+              usremail={userReducer.email}
+              backendUrl={backendUrl}
+              changeTitle={this.changeTitle}
+            />
+          }
+        />
 
-                <Route
-                    path={`${match.url}/livechat/:topicId`}
-                    render={
-                        props => <Livechat
-                            {...props}
-                            jwt={jwt}
-                            backendUrl={backendUrl}
-                            userReducer={userReducer}
-                            changeTitle={this.changeTitle}
-                        />
-                    }
-                />
+        <Route
+          path={`${match.url}/livechat/:topicId`}
+          render={
+            props => <Livechat
+              {...props}
+              jwt={jwt}
+              backendUrl={backendUrl}
+              userReducer={userReducer}
+              changeTitle={this.changeTitle}
+            />
+          }
+        />
 
-            </Container>
-        )
-    }
+      </Container>
+    )
+  }
 }
 
 export default Console
